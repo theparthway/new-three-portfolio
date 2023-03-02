8672729c4f1fed7ab85d02f849b352adc04ffe03
theparthway <parthpsh@gmail.com>
2023-03-02 17:38:52 +0530

    sierpinski's triangle

---
+++ viz/sierpinski/script.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(51);
+    stroke(255);
+    noFill();
+}
+
+function draw() {
+    triangle(x1, y1, x2, y2, x3, y3)
+}
